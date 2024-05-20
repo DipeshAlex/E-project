761641617779d49a6842dd7cf3e314734fef602b
DipeshAlex <bhoidipesh217@gmail.com>
2024-05-21 00:09:34 +0530

    first commit

---
+++ website/footer.php
@@ -0,0 +1,226 @@
+<!-- end section -->
+<!-- Modal -->
+<div class="modal fade" id="search_bar" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><i class="fa fa-close"></i></button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-lg-8 col-md-8 col-sm-8 offset-lg-2 offset-md-2 offset-sm-2 col-xs-10 col-xs-offset-1">
+            <div class="navbar-search">
+              <form action="#" method="get" id="search-global-form" class="search-global">
+                <input type="text" placeholder="Type to search" autocomplete="off" name="s" id="search" value="" class="search-global__input">
+                <button class="search-global__btn"><i class="fa fa-search"></i></button>
+                <div class="search-global__note">Begin typing your search above and press return to search.</div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End Model search bar -->
+<!-- footer -->
+<footer class="footer_style_2">
+  <div class="container-fuild">
+    <div class="row">
+      <div class="map_section">
+        <div id="map"></div>
+      </div>
+      <div class="footer_blog">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="main-heading left_text">
+              <h2>It Next Theme</h2>
+            </div>
+            <p>Tincidunt elit magnis nulla facilisis. Dolor sagittis maecenas. Sapien nunc amet ultrices, dolores sit ipsum velit purus aliquet, massa fringilla leo orci.</p>
+            <ul class="social_icons">
+              <li class="social-icon fb"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li class="social-icon tw"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+              <li class="social-icon gp"><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+          <div class="col-md-6">
+            <div class="main-heading left_text">
+              <h2>Additional links</h2>
+            </div>
+            <ul class="footer-menu">
+              <li><a href="it_about.php"><i class="fa fa-angle-right"></i> About us</a></li>
+              <li><a href="it_term_condition.php"><i class="fa fa-angle-right"></i> Terms and conditions</a></li>
+              <li><a href="it_privacy_policy.php"><i class="fa fa-angle-right"></i> Privacy policy</a></li>
+              <li><a href="it_news.php"><i class="fa fa-angle-right"></i> News</a></li>
+              <li><a href="it_contact.php"><i class="fa fa-angle-right"></i> Contact us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-6">
+            <div class="main-heading left_text">
+              <h2>Services</h2>
+            </div>
+            <ul class="footer-menu">
+              <li><a href="it_data_recovery.php"><i class="fa fa-angle-right"></i> Data recovery</a></li>
+              <li><a href="it_computer_repair.php"><i class="fa fa-angle-right"></i> Computer repair</a></li>
+              <li><a href="it_mobile_service.php"><i class="fa fa-angle-right"></i> Mobile service</a></li>
+              <li><a href="it_network_solution.php"><i class="fa fa-angle-right"></i> Network solutions</a></li>
+              <li><a href="it_techn_support.php"><i class="fa fa-angle-right"></i> Technical support</a></li>
+            </ul>
+          </div>
+          <div class="col-md-6">
+            <div class="main-heading left_text">
+              <h2>Contact us</h2>
+            </div>
+            <p>123 Second Street Fifth Avenue,<br>
+              Manhattan, New York<br>
+              <span style="font-size:18px;"><a href="tel:+9876543210">+987 654 3210</a></span></p>
+            <div class="footer_mail-section">
+              <form>
+                <fieldset>
+                <div class="field">
+                  <input placeholder="Email" type="text">
+                  <button class="button_custom"><i class="fa fa-envelope" aria-hidden="true"></i></button>
+                </div>
+                </fieldset>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="cprt">
+        <p>ItNext © Copyrights 2019 Design by html.design</p>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- end footer -->
+<!-- js section -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- menu js -->
+<script src="js/menumaker.js"></script>
+<!-- wow animation -->
+<script src="js/wow.js"></script>
+<!-- custom js -->
+<script src="js/custom.js"></script>
+<!-- revolution js files -->
+<script src="revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="revolution/js/jquery.themepunch.revolution.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.video.min.js"></script>
+<!-- map js -->
+<script>
+         // This example adds a marker to indicate the position of Bondi Beach in Sydney,
+         // Australia.
+         function initMap() {
+           var map = new google.maps.Map(document.getElementById('map'), {
+             zoom: 11,
+             center: {lat: 40.645037, lng: -73.880224},
+         styles: [
+                  {
+                    elementType: 'geometry',
+                    stylers: [{color: '#fefefe'}]
+                  },
+                  {
+                    elementType: 'labels.icon',
+                    stylers: [{visibility: 'off'}]
+                  },
+                  {
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#616161'}]
+                  },
+                  {
+                    elementType: 'labels.text.stroke',
+                    stylers: [{color: '#f5f5f5'}]
+                  },
+                  {
+                    featureType: 'administrative.land_parcel',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#bdbdbd'}]
+                  },
+                  {
+                    featureType: 'poi',
+                    elementType: 'geometry',
+                    stylers: [{color: '#eeeeee'}]
+                  },
+                  {
+                    featureType: 'poi',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#757575'}]
+                  },
+                  {
+                    featureType: 'poi.park',
+                    elementType: 'geometry',
+                    stylers: [{color: '#e5e5e5'}]
+                  },
+                  {
+                    featureType: 'poi.park',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#9e9e9e'}]
+                  },
+                  {
+                    featureType: 'road',
+                    elementType: 'geometry',
+                    stylers: [{color: '#eee'}]
+                  },
+                  {
+                    featureType: 'road.arterial',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#3d3523'}]
+                  },
+                  {
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [{color: '#eee'}]
+                  },
+                  {
+                    featureType: 'road.highway',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#616161'}]
+                  },
+                  {
+                    featureType: 'road.local',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#9e9e9e'}]
+                  },
+                  {
+                    featureType: 'transit.line',
+                    elementType: 'geometry',
+                    stylers: [{color: '#e5e5e5'}]
+                  },
+                  {
+                    featureType: 'transit.station',
+                    elementType: 'geometry',
+                    stylers: [{color: '#000'}]
+                  },
+                  {
+                    featureType: 'water',
+                    elementType: 'geometry',
+                    stylers: [{color: '#c8d7d4'}]
+                  },
+                  {
+                    featureType: 'water',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#b1a481'}]
+                  }
+                ]
+         });
+         
+           var image = 'images/it_service/location_icon_map_cont.png';
+           var beachMarker = new google.maps.Marker({
+             position: {lat: 40.645037, lng: -73.880224},
+             map: map,
+             icon: image
+           });
+         }
+      </script>
+<!-- google map js -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8eaHt9Dh5H57Zh0xVTqxVdBFCvFMqFjQ&callback=initMap"></script>
---
+++ website/index.php
@@ -0,0 +1,215 @@
+<?php
+include_once('header.php');
+?>
+<!-- section -->
+<!-- end section -->
+<!-- section -->
+<div class="section padding_layout_1">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="full">
+          <div class="main_heading text_align_center">
+            <h2>Why Choose Us</h2>
+            <p class="large">"Better education develops the nation"</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
+<!-- end section -->
+<!-- section -->
+<div class="section padding_layout_1 light_silver gross_layout">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="full">
+          <div class="main_heading text_align_left">
+            <h2>Service Process</h2>
+            <p class="large">Easy and effective way to get your device repaired.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-8">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="full">
+              <div class="service_blog_inner">
+                <div class="icon text_align_left"><img src="images/it_service/si1.png" alt="#" /></div>
+                <h4 class="service-heading">Fast service</h4>
+                <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="full">
+              <div class="service_blog_inner">
+                <div class="icon text_align_left"><img src="images/it_service/si2.png" alt="#" /></div>
+                <h4 class="service-heading">Secure payments</h4>
+                <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo.</p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-md-4">
+            <div class="full">
+              <div class="service_blog_inner">
+                <div class="icon text_align_left"><img src="images/it_service/si4.png" alt="#" /></div>
+                <h4 class="service-heading">Affordable services</h4>
+                <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo.</p>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- end section -->
+<!-- section -->
+<div class="section padding_layout_1">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="full">
+          <div class="main_heading text_align_center">
+            <h2>Our Products</h2>
+            <p class="large">We package the products with best services to make you a happy customer.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 margin_bottom_30_all">
+        <div class="product_list">
+          <div class="product_img"> <img class="img-responsive" src="images\it_service\1.jpg" alt=""> </div>
+          <div class="product_detail_btm">
+            <div class="center">
+              <h4><a href="shop_detail.php">Python</a></h4>
+            </div>
+            <div class="starratin">
+              <div class="center"> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </div>
+            </div>
+            <div class="product_price">
+              <p><span class="old_price"></span>  <span class="new_price"> Rs.10,000</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 margin_bottom_30_all">
+        <div class="product_list">
+          <div class="product_img"> <img class="img-responsive" src="images/it_service/2.jpg" alt=""> </div>
+          <div class="product_detail_btm">
+            <div class="center">
+              <h4><a href="shop_detail.php">Javascript</a></h4>
+            </div>
+            <div class="starratin">
+              <div class="center"> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </div>
+            </div>
+            <div class="product_price">
+              <p><span class="old_price"></span><span class="new_price">Rs.5000</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 margin_bottom_30_all">
+        <div class="product_list">
+          <div class="product_img"> <img class="img-responsive" src="images/it_service/3.jpg" alt=""> </div>
+          <div class="product_detail_btm">
+            <div class="center">
+              <h4><a href="shop_detail.php">Ruby</a></h4>
+            </div>
+            <div class="starratin">
+              <div class="center"> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </div>
+            </div>
+            <div class="product_price">
+              <p><span class="old_price"></span><span class="new_price">Rs.15,500</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 margin_bottom_30_all">
+        <div class="product_list">
+          <div class="product_img"> <img class="img-responsive" src="images/it_service/4.jpg" alt=""> </div>
+          <div class="product_detail_btm">
+            <div class="center">
+              <h4><a href="shop_detail.php">Sql</a></h4>
+            </div>
+            <div class="starratin">
+              <div class="center"> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </div>
+            </div>
+            <div class="product_price">
+              <p><span class="old_price"></span><span class="new_price">Rs.2500</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      </div>
+      </div>
+       
+
+<!-- end section -->
+<!-- section -->
+<div class="section padding_layout_1 light_silver gross_layout right_gross_layout">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="full">
+          <div class="main_heading text_align_right">
+            <h2>Our Feedback</h2>
+            <p class="large">Easy and effective way to get your device repaired.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row counter">
+      <div class="col-md-4"> </div>
+      <div class="col-md-8">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 margin_bottom_50">
+            <div class="text_align_right"><i class="fa fa-smile-o"></i></div>
+            <div class="text_align_right">
+              <p class="counter-heading text_align_right">Happy Customers</p>
+            </div>
+            <h5 class="counter-count">2150</h5>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 margin_bottom_50">
+            <div class="text_align_right"><i class="fa fa-laptop"></i></div>
+            <div class="text_align_right">
+              <p class="counter-heading text_align_right">Laptop Repaired</p>
+            </div>
+            <h5 class="counter-count">1280</h5>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 margin_bottom_50">
+            <div class="text_align_right"><i class="fa fa-desktop"></i></div>
+            <div class="text_align_right">
+              <p class="counter-heading">Computer Repaired</p>
+            </div>
+            <h5 class="counter-count">848</h5>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 margin_bottom_50">
+            <div class="text_align_right"><i class="fa fa-windows"></i></div>
+            <div class="text_align_right">
+              <p class="counter-heading">OS Installed</p>
+            </div>
+            <h5 class="counter-count">450</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- end section -->
+<!-- section -->
+<div class="section padding_layout_1"> </div>
+<!-- end section -->
+<?php
+include_once('footer.php');
+?>
